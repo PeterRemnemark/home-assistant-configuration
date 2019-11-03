d18afce1841f3d6b2355b8526dc2518ef543be7d
Peter Remnemark <peter@prphoto.se>
2019-11-03 10:24:26 +0100

    Added more lights and replaces one switch with light

--- custom_components/alexa_media/const.py
+++ custom_components/alexa_media/const.py
@@ -1,11 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#  SPDX-License-Identifier: Apache-2.0
 """
 Support to interface with Alexa Devices.
 
 For more details about this platform, please refer to the documentation at
 https://community.home-assistant.io/t/echo-devices-alexa-as-media-player-testers-needed/58639
-VERSION 1.0.0
 """
 from datetime import timedelta
+
+__version__ = '2.3.4'
+PROJECT_URL = "https://github.com/custom-components/alexa_media_player/"
+ISSUE_URL = "{}issues".format(PROJECT_URL)
 
 DOMAIN = 'alexa_media'
 DATA_ALEXAMEDIA = 'alexa_media'
@@ -16,12 +22,29 @@ MIN_TIME_BETWEEN_SCANS = SCAN_INTERVAL
 MIN_TIME_BETWEEN_FORCED_SCANS = timedelta(seconds=1)
 
 ALEXA_COMPONENTS = [
-    'media_player'
+    'media_player',
+    'notify',
+    'alarm_control_panel',
+    'switch',
+    'sensor'
 ]
 
 CONF_ACCOUNTS = 'accounts'
 CONF_DEBUG = 'debug'
 CONF_INCLUDE_DEVICES = 'include_devices'
 CONF_EXCLUDE_DEVICES = 'exclude_devices'
-SERVICE_ALEXA_TTS = 'alexa_tts'
+
+SERVICE_CLEAR_HISTORY = 'clear_history'
+SERVICE_UPDATE_LAST_CALLED = 'update_last_called'
 ATTR_MESSAGE = 'message'
+ATTR_EMAIL = 'email'
+ATTR_NUM_ENTRIES = 'entries'
+STARTUP = """
+-------------------------------------------------------------------
+{}
+Version: {}
+This is a custom component
+If you have any issues with this you need to open an issue here:
+{}
+-------------------------------------------------------------------
+""".format(DOMAIN, __version__, ISSUE_URL)
